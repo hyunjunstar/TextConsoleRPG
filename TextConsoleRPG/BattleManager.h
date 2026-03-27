4d1cb578d39edce25ca3ae2f43ada52d1a6376f4
IOI\User <happiness2135@gmail.com>
2026-03-27 15:03:21 +0900

    헤더파일 안 쓰는 함수 삭제 및 주석 영어 변환 약간 수정했습니다.

--- TextConsoleRPG/BattleManager.h
+++ TextConsoleRPG/BattleManager.h
@@ -6,18 +6,15 @@
 class BattleManager
 {
 private:
-	// 유지보수를 위한 크리티컬 및 데미지 변동성 상수화
+	// Constant Critical and Damage Variability for Maintenance
 	static constexpr int CRIT_MAX_CHANCE = 100;
 	static constexpr float CRIT_DAMAGE_MULTIPLIER = 2.0f;
 	static constexpr int VARIANCE_DIVIDER = 5;
 
 public:
-    BattleManager() = default;
-    ~BattleManager() = default;
+	BattleManager() = default;
+	~BattleManager() = default;
 
-    // 1턴 동안의 동작 정리
-    // void processTurn(Player& player, Monster& monster);
-
-    // 끝날 때까지 processTurn 돌리기
-    // bool runBattle(Player& player, Monster& monster);
+	// 1 turn behavior 
+	// void processTurn(Player& player, Monster& monster);
 };
