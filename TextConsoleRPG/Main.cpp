6c34af23559601d8e8d5ee3d8a1e40c06bee20ef
BRD\brogd <brogdam208@kakao.com>
2026-03-27 09:28:17 +0900

    [Stage 2], [Stage 3] 추가 및 엔딩 연출 추가

--- TextConsoleRPG/Main.cpp
+++ TextConsoleRPG/Main.cpp
@@ -64,6 +64,67 @@ int main(void)
 	//* myPlayer.showStatus();
 	//* NextStep("다음 스테이지로 이동하려면 엔터를 누르세요.");
 	
-	
+		//* Monster Goblin("고블린", 120, 10); // 체력 120, 공격력 10
+	//* cout << "[Stage 2] " << Goblin.name << "이(가) 기습해옵니다!" << endl;
+
+	//* while (!myPlayer.isDead() && !Goblin.isDead())
+	//* {
+		//* battle.processTurn(myPlayer, Goblin);
+
+		//* if (!myPlayer.isDead() && !Goblin.isDead())
+		//* {
+			//* NextStep("계속 싸우려면 엔터를 누르세요.");
+		//* }
+	//* }
+
+	//* if (myPlayer.isDead())
+	//* {
+		//* cout << "으악...!" << endl;
+		//* cout << "===================" << endl;
+		//* cout << "GAME OVER" << endl;
+
+		//* return 0;
+	//* }
+
+	//* cout << Goblin.name << "을 물리쳤습니다!" << endl;
+	//* artifactSystem.chooseAndApply(myPlayer);
+	//* myPlayer.showStatus();
+	//* NextStep("보스방으로 진입하려면 엔터를 누르세요.");
+
+
+	//* Monster Orc("최종보스 오크", 300, 20); // 체력 300, 공격력 20
+	//* cout << "[Final Stage] " << Orc.name << "이(가) 등장했습니다!!" << endl;
+
+	//* while (!myPlayer.isDead() && !Orc.isDead())
+	//* {
+		//* battle.processTurn(myPlayer, Orc);
+
+		//* if (!myPlayer.isDead() && !Orc.isDead())
+		//* {
+			//* NextStep("계속 싸우려면 엔터를 누르세요.");
+		//* }
+	//* }
+
+	//* if (myPlayer.isDead())
+	//* {
+		//* cout << "으악...!" << endl;
+		//* cout << "===================" << endl;
+		//* cout << "GAME OVER" << endl;
+
+		//* return 0;
+	//* }
+
+	//* cout << Orc.name << "을 물리치고 마을의 평화를 되찾았습니다!" << endl;
+	//* cout << "\n\n****************************************" << endl;
+	//* cout << "Game Clear!" << endl;
+	//* cout << "****************************************\n\n" << endl;
+	//* cout << "\n\n [만든 사람들]" << endl;
+	//* cout << "- 메인 디렉터 : 백록담 - " << endl;
+	//* cout << "- 플레이어 담당 : 김남윤 - " << endl;
+	//* cout << "- 몬스터 담당 : 김현준 - " << endl;
+	//* cout << "- 핵심 로직(전투 계산) 담당 : 조유현 - " << endl;
+	//* cout << "- 성장 로직(유물 시스템) 담당 : 정태민 - " << endl;
+	//* cout << "- UI 및 텍스트 연출 담당 : 정태민 - " << endl;
+
 	return 0;
 }
