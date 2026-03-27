41a8e7c4506be0287509c786ae7042ba11e943b6
LAPTOP-TIS2U9KE\JW <jjw77889900@gmail.com>
2026-03-27 15:27:00 +0900

    UI.cpp (텍스트그림 출력함수) 추가

---
+++ TextConsoleRPG/UI.h
@@ -0,0 +1,16 @@
+#pragma once
+class UI
+{
+public:
+	void printGameOver();
+	void printGameClear();
+
+	void printCharacter();
+
+	void printSlime();
+	void printGoblin();
+	void printOrk();
+
+	void printAttack();
+};
+
